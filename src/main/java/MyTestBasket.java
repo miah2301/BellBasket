50db3bc5defff1681cf3ed4edf89316b23f915b6
Mihail Seregin <mihail.mieke@yandex.ru>
2022-11-30 19:31:22 +0300

    Completed

--- src/main/java/MyTestBasket.java
+++ src/main/java/MyTestBasket.java
@@ -1,22 +1,22 @@
 public class MyTestBasket {
     public static void main(String[] args) {
-        BasketStore basketOnlineStore = new BasketStore();
-        basketOnlineStore.addProduct("product1", 10);
-        basketOnlineStore.addProduct("product2", 20);
-        basketOnlineStore.addProduct("product3", 30);
-        basketOnlineStore.addProduct("product4", 40);
+        BasketStore basketStore = new BasketStore();
+        basketStore.addProduct("product1", 10);
+        basketStore.addProduct("product2", 20);
+        basketStore.addProduct("product3", 30);
+        basketStore.addProduct("product4", 40);
 
-        for (String product : basketOnlineStore.getProducts()) {
-            System.out.println(product + " " + basketOnlineStore.getProductQuantity(product));
+        for (String product : basketStore.getProducts()) {
+            System.out.println(product + " " + basketStore.getProductQuantity(product));
         }
-        System.out.println(basketOnlineStore.getProducts());
+        System.out.println(basketStore.getProducts());
 
-        basketOnlineStore.removeProduct("product4");
-        basketOnlineStore.updateProductQuantity("product1", 777);
+        basketStore.removeProduct("product4");
+        basketStore.updateProductQuantity("product1", 777);
 
-        for (String product : basketOnlineStore.getProducts()) {
-            System.out.println(product + " " + basketOnlineStore.getProductQuantity(product));
+        for (String product : basketStore.getProducts()) {
+            System.out.println(product + " " + basketStore.getProductQuantity(product));
         }
-        System.out.println(basketOnlineStore.getProducts());
+        System.out.println(basketStore.getProducts());
     }
 }
